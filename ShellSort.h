496cc209fbc687a21f682b62063baf7720a4bb9e
NeloGino <nelogino@gmail.com>
2017-11-20 01:02:53 -0300

    Casi shellsort: 0123456798

--- ShellSort.h
+++ ShellSort.h
@@ -5,12 +5,13 @@
 template<class T>
 void shellSort(T *vect, unsigned long tamanio) {        //tamanio en lenguaje programador
     for (auto k = tamanio / 2; k > 0; k /= 2) {         // k incremento
-        for (auto i = k; i < tamanio; i++) {            // i vuelta
-            for (auto j = i; j >= k && vect[j - k] > vect[i]; j -= k) {
-                T aux = vect[i];
-                vect[i] = vect[j - k];
-                vect[j - k] = aux;
+        for (auto i = k; i < tamanio; ++i) {            // i vuelta
+            T temp = vect[i];                           // temp porque va cambiando
+            auto j = i;
+            for (j = i; j >= k && vect[j - k] > temp; j -= k) {
+                vect[j] = vect[j - k];
             }
+            vect[j] = temp;
         }
     }
 }
